2ab7a34d4a5e5a3676e1bdf12bb40ad20c52f4eb
SvenAlHamad <sven@webiny.com>
2015-02-05 03:38:25 +0000

    Version bumped to 1.2.1

--- Tests/OAuth2LoaderTest.php
+++ Tests/OAuth2LoaderTest.php
@@ -7,10 +7,14 @@
 
 namespace Webiny\Component\OAuth2\Tests;
 
+use Webiny\Component\Http\Request;
 use Webiny\Component\OAuth2\OAuth2;
 use Webiny\Component\OAuth2\OAuth2Loader;
 
-
+/**
+ * Class OAuth2LoaderTest
+ * @package Webiny\Component\OAuth2\Tests
+ */
 class OAuth2LoaderTest extends \PHPUnit_Framework_TestCase
 {
 
@@ -19,19 +23,6 @@ class OAuth2LoaderTest extends \PHPUnit_Framework_TestCase
     public function testGetInstance()
     {
         OAuth2::setConfig(realpath(__DIR__ . '/' . self::CONFIG));
-
-        // we need to mock the $_SERVER
-        $_SERVER = [
-            'USER'            => 'webiny',
-            'HOME'            => '/home/webiny',
-            'SCRIPT_FILENAME' => '/var/www/projects/webiny/Public/index.php',
-            'SCRIPT_NAME'     => '/index.php',
-            'REQUEST_URI'     => '/batman-is-better-than-superman/?batman=one&superman=two',
-            'DOCUMENT_URI'    => '/index.php',
-            'SERVER_PROTOCOL' => 'HTTP/1.1',
-            'REMOTE_ADDR'     => '192.168.58.1',
-            'SERVER_NAME'     => 'admin.w3.com',
-        ];
 
         $instance = OAuth2Loader::getInstance('Facebook');
         $this->assertInstanceOf('\Webiny\Component\OAuth2\OAuth2', $instance);
